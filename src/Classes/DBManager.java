48935297540bef39bbd8ba7e4d3fbff709c21118
StuartR <StuartR@192.168.1.2>
2017-12-12 12:01:48 +0000

    crud methods for product classes complete - DBManager update

--- src/Classes/DBManager.java
+++ src/Classes/DBManager.java
@@ -86,6 +86,213 @@ public class DBManager {
         }
     }
     
+    //this method deletes a single product entry
+    public void deleteProduct (Product prod)
+    {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
+                Statement stmt = conn.createStatement();
+                
+            if(prod instanceof Clothing)
+                {
+                    stmt.executeUpdate("DELETE FROM PRODUCTSTABLE WHERE PRODUCTID = '" + prod.getProductID() + "'; DELETE FROM CLOTHINGTABLE WHERE PRODUCTID = " + prod.getProductID() + "'");
+                    
+                }
+                else
+                {
+                    stmt.executeUpdate("DELETE FROM PRODUCTSTABLE WHERE PRODUCTID = '" + prod.getProductID() + "'; DELETE FROM FOOTWEARTABLE WHERE PRODUCTID = " + prod.getProductID() + "'");
+                }
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            String message = ex.getMessage();
+            System.out.println(message);
+        }
+    }
+    
+    
+    /**
+     *This method cycles through our database and stores all Clothing entries to our HashMap
+     * @return Method returns our HashMap of Clothing items
+     */
+    public HashMap<Integer, Clothing> loadAllClothing()
+    {
+        HashMap<Integer, Clothing> clothes = new HashMap();
+        
+        
+        try 
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
+                Statement stmt = conn.createStatement();
+                String sql = "Select * From CLOTHINGTABLE INNER JOIN PRODUCTSTABLE ON CLOTHINGTABLE.PRODUCTID = PRODUCTSTABLE.PRODUCTID";
+                ResultSet rst;
+                rst = stmt.executeQuery(sql);
+                
+                while (rst.next())
+                {
+                    Clothing cloth = new Clothing();
+                    cloth.setProductID(rst.getInt("PRODUCTID"));
+                    cloth.setProductName(rst.getString("PRODUCTNAME"));
+                    cloth.setPrice(rst.getDouble("PRICE"));
+                    cloth.setStockLevel(rst.getInt("STOCKLEVEL"));
+                    cloth.setMeasurement(rst.getString("MEASUREMENT"));
+                    clothes.put(cloth.getProductID(), cloth);
+                }
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            String message = ex.getMessage();
+            System.out.println(message);
+        }
+        return clothes;
+    }
+    
+    
+    /**
+     *This method cycles through our database and stores all Footwear entries to our HashMap
+     * @return Method returns our HashMap of Footwear items
+     */
+    public HashMap<Integer, Footwear> loadAllFootwear()
+    {
+        HashMap<Integer, Footwear> footwear = new HashMap();
+        
+        
+        try 
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
+                Statement stmt = conn.createStatement();
+                String sql = "Select * From FOOTWEARTABLE INNER JOIN PRODUCTSTABLE ON FOOTWEARTABLE.PRODUCTID = PRODUCTSTABLE.PRODUCTID";
+                ResultSet rst;
+                rst = stmt.executeQuery(sql);
+                
+                while (rst.next())
+                {
+                    Footwear foot = new Footwear();
+                    foot.setProductID(rst.getInt("PRODUCTID"));
+                    foot.setProductName(rst.getString("PRODUCTNAME"));
+                    foot.setPrice(rst.getDouble("PRICE"));
+                    foot.setStockLevel(rst.getInt("STOCKLEVEL"));
+                    foot.setSize(rst.getInt("SIZE"));
+                    footwear.put(foot.getProductID(), foot);
+                }
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            String message = ex.getMessage();
+            System.out.println(message);
+        }
+        return footwear;
+    }
+    
+    
+    /**
+     * This method takes in two parameters and uses them to update an entry in our database
+     * @param originalCloth
+     * @param newCloth 
+     */
+        public void updateClothingItem (Clothing originalCloth, Clothing newCloth)
+    {
+        try {
+                Class.forName("org.apache.derby.jdbc.ClientDriver");
+                try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate("UPDATE PRODUCTSTABLE SET PROCUCTNAME = '" + newCloth.getProductName() + "', PRICE = '" + newCloth.getPrice() + "', STOCKLEVEL = '" + newCloth.getStockLevel() + "' WHERE PRODUCTID = '" + newCloth.getProductID() + "'");
+                    stmt.executeUpdate("UPDATE CLOTHINGTABLE SET MEASUREMENT = '" + newCloth.getMeasurement() + "' WHERE PRODUCTID = '" + newCloth.getProductID() + "'");
+                }
+
+            } catch (ClassNotFoundException | SQLException ex) {
+                String message = ex.getMessage();
+                System.out.println(message);
+            }
+    }
+    
+    /**
+     * This method takes in two parameters and uses them to update an entry in our database
+     * @param originalFoot
+     * @param newFoot
+     */
+        public void updateFootwearItem (Footwear originalFoot, Footwear newFoot)
+    {
+        try {
+                Class.forName("org.apache.derby.jdbc.ClientDriver");
+                try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate("UPDATE PRODUCTSTABLE SET PROCUCTNAME = '" + newFoot.getProductName() + "', PRICE = '" + newFoot.getPrice() + "', STOCKLEVEL = '" + newFoot.getStockLevel() + "' WHERE PRODUCTID = '" + originalFoot.getProductID() + "'");
+                    stmt.executeUpdate("UPDATE FOOTWEARTABLE SET SIZE = '" + newFoot.getSize() + "' WHERE PRODUCTID = '" + originalFoot.getProductID() + "'");
+                }
+
+            } catch (ClassNotFoundException | SQLException ex) {
+                String message = ex.getMessage();
+                System.out.println(message);
+            }
+    }
+    
+        
+    public Clothing findClothing(int id)
+    {
+    Clothing cloth = new Clothing();
+        try 
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234");
+            Statement stmt = conn.createStatement();
+            String sql = "Select * From CLOTHINGTABLE INNER JOIN PRODUCTSTABLE ON CLOTHINGTABLE.PRODUCTID = PRODUCTSTABLE.PRODUCTID WHERE PRODUCTID = '" + id + "'";
+            ResultSet rst;
+            rst = stmt.executeQuery(sql);
+                
+            if(rst.next())
+            {
+                cloth.setProductID(rst.getInt("PRODUCTID"));
+                cloth.setProductName(rst.getString("PRODUCTNAME"));
+                cloth.setPrice(rst.getDouble("PRICE"));
+                cloth.setStockLevel(rst.getInt("STOCKLEVEL"));
+                cloth.setMeasurement(rst.getString("MEASUREMENT"));
+            }
+            else
+            {
+                cloth = null;
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            String message = ex.getMessage();
+            System.out.println(message);
+        }
+        return cloth;
+    }
+        
+    
+    public Footwear findFootwear(int id)
+    {
+    Footwear foot = new Footwear();
+        try 
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234");
+            Statement stmt = conn.createStatement();
+            String sql = "Select * From FOOTWEARTABLE INNER JOIN PRODUCTSTABLE ON FOOTWEARTABLE.PRODUCTID = PRODUCTSTABLE.PRODUCTID WHERE PRODUCTID = '" + id + "'";
+            ResultSet rst;
+            rst = stmt.executeQuery(sql);
+                
+            if(rst.next())
+            {
+                foot.setProductID(rst.getInt("PRODUCTID"));
+                foot.setProductName(rst.getString("PRODUCTNAME"));
+                foot.setPrice(rst.getDouble("PRICE"));
+                foot.setStockLevel(rst.getInt("STOCKLEVEL"));
+                foot.setSize(rst.getInt("SIZE"));
+            }
+            else
+            {
+                foot = null;
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            String message = ex.getMessage();
+            System.out.println(message);
+        }
+        return foot;
+    }
+    
+    
+    
     
     
     
@@ -143,7 +350,7 @@ public class DBManager {
     
     
     
-       public void deleteAllCustomers() {
+    public void deleteAllCustomers() {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
@@ -160,9 +367,9 @@ public class DBManager {
        
        
        
-        public boolean deleteCustomer (Customer originalCust)
+    public void deleteCustomer (Customer originalCust)
     {
-        boolean deleted = false;
+
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CITYSHOPPINGDB","Stuart", "1234")) {
@@ -174,20 +381,18 @@ public class DBManager {
             String message = ex.getMessage();
             System.out.println(message);
         }
-        return deleted;
     }
     
         
         
 
     /**
-     *This method cycles through our database and stores all entries to our NootBook
-    * @return Method returns an instance of the NoteBook class
+     *This method cycles through our database and stores all entries to our HashMap
+    * @return Method returns our HashMap variable
     */
     public HashMap<String, Customer> loadAllCustomers()
     {
         HashMap<String, Customer> customers = new HashMap();
-        
         
         try 
         {
