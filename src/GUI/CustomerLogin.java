035e3a53cb69bd3db0a40d975218fe77bb0d8b13
angel <angel@10.97.34.232>
2018-02-18 02:46:34 +0000

    commented Customer Login GUI class

--- src/GUI/CustomerLogin.java
+++ src/GUI/CustomerLogin.java
@@ -13,30 +13,40 @@ import javax.swing.JOptionPane;
 
 /**
  * @date 13/11/2017
+ * @date 18/2/2018 - comments added sr
  * @author Stuart Reid
  * Class defining an instance of our login page.
  */
 public class CustomerLogin extends javax.swing.JFrame {
 
     //attributes
+    
+    //gloabal variable to store our logged in customer
     Customer cust;
+    //hashmap global variable holding customers basket
     HashMap<Integer, OrderLine> basket;
+    //boolean tracking if page was opened via the ViewBasket page
     boolean fromBasket = false;
+    
     
     /**
      * Default constructor that creates an empty instance of our CustomerLogin screen
+     * initialises components
+     * sets customer to null
      */
     public CustomerLogin() {
         initComponents();
         cust = null;
     }
+    
+    
     /**
-     * Overloaded Constructor passing in Customer instance and HashMap of Customers
-     * @param cust this parameter is an instance of the customer class and is passed to the page if there is currently a customer logged in
+     * Overloaded constructor creating an instance our our form
+     * initialises components
+     * sets from basket to true as it is only used via the View Basket screen
+     * changes the text of our main menu button
+     * @param basket - variable holding a hashmap of orderlines
      */
-    public CustomerLogin(Customer cust){initComponents(); this.cust = cust;}
-    
-    
     public CustomerLogin(HashMap<Integer, OrderLine> basket)
     {
         initComponents();
@@ -45,7 +55,15 @@ public class CustomerLogin extends javax.swing.JFrame {
         fromBasket = true;  
     }
 
-    
+    /**
+     * Overloaded constructor creating an instance our our form
+     * initialises components
+     * maps basket to passed in parameter
+     * maps from basket to passed in parameter
+     * changes the text of our main menu button
+     * @param basket - variable holding a hashmap of orderlines
+     * @param fromBasket - a boolean value tracking wither the page has been redirected via the view basket page
+     */
     public CustomerLogin(boolean fromBasket, HashMap<Integer, OrderLine> basket)
     {
         initComponents();
@@ -80,12 +98,6 @@ public class CustomerLogin extends javax.swing.JFrame {
         titleLbl.setText("CUSTOMER LOGIN");
 
         userNameLbl.setText("USERNAME:");
-
-        userNameTxtBx.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                userNameTxtBxActionPerformed(evt);
-            }
-        });
 
         passwordLbl.setText("PASSWORD:");
 
@@ -157,17 +169,25 @@ public class CustomerLogin extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void userNameTxtBxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userNameTxtBxActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_userNameTxtBxActionPerformed
-
+    
+    /**
+     * on click for log in button
+     * @param evt 
+     */
     private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
-        //on click for Login button - we will take in user input and search DB for details matching input
+        //creating an instance of our database manager
         DBManager db = new DBManager();
-        String inputUsername = userNameTxtBx.getText().toLowerCase();
-        Customer myCust = db.findCustomer(inputUsername); 
+        
+        //getting user input and storing it to a string
+        String inputUserName = userNameTxtBx.getText().toLowerCase();
+        
+        //creating an instance ouf our customer class and setting it to the object returned via the dbmanager
+        Customer myCust = db.findCustomer(inputUserName); 
+        
+        //if the value is null
         if (myCust == null)
         {
+            //error message
             titleLbl.setText("Username or Password incorrect!");
         }
         else
@@ -178,73 +198,114 @@ public class CustomerLogin extends javax.swing.JFrame {
             char[] validPassword = myCust.getPassword().toCharArray();
             //comparing arrays and returning true or fasle
             boolean pwCorrect = Arrays.equals(passwordInput, validPassword);
-            String inputUserName = userNameTxtBx.getText();
-            String userName = myCust.getUsername();
-            boolean unCorrect = (inputUserName.equalsIgnoreCase(userName));
             
-            if (unCorrect && pwCorrect)
+            //if the input password matches the one held in the database
+            if (pwCorrect)
             {
+                //if the page was not redirected from the viewbasket
                 if(!fromBasket)
                 {
+                    //stores logged in customer to global variable
                     cust = myCust;
+                    //creates new instance of customer home page passing in cust
                     CustomerHome home = new CustomerHome(cust);
-                    home.setVisible(true);
+                    //closes existing form
                     this.dispose();
+                    //sets new form to visible
+                    home.setVisible(true);
                 }
+                //if the page was redirected from the viewbasket
                 else
                 {
+                    //maps customer global variable to our logged in user
                     cust = myCust;
+                    //pop up confirmation displayed
                     infoBox("You are now logged in!","LOGGED IN");
+                    //creates new instance of our view basket form passing in the logged in customer and the basket hashmap
                     ViewBasket vb = new ViewBasket(cust, basket);
+                    //closes existing form
                     this.dispose();
+                    //sets new form to visible
                     vb.setVisible(true);
                 }
             }
+            //if password doesnt match
             else
             {
+                //error message displayed
                 titleLbl.setText("Username or Password incorrect!");
             }
         }      
     }//GEN-LAST:event_loginBtnActionPerformed
 
+    
+    /**
+     * on click for our main menu button
+     * @param evt 
+     */
     private void mainMenuBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mainMenuBtnActionPerformed
-        // TODO add your handling code here:
+        // if customer is not null
         if (cust != null)
         {
+            //creates a main menu passing in logged in customer
             Menu menu = new Menu(cust);
+            //closes existing form
             this.dispose();
+            //sets new form to visible
             menu.setVisible(true);
         }
+        //if redirected via basket
         if(fromBasket)
         {
+            //creates new instance of our view basket form passing in the logged in customer and the basket hashmap
             ViewBasket vb = new ViewBasket(cust, basket);
+            //closes existing form
             this.dispose();
+            //sets new form to visible
             vb.setVisible(true);
         }
+        // if no logged in user and not redirected via the basket
         else
         {
+            //creates instance of our main menu form
             Menu menu = new Menu();
+            //closes existing form
             this.dispose();
+            //sets new form to visible
             menu.setVisible(true);
         }
     }//GEN-LAST:event_mainMenuBtnActionPerformed
 
+    
+    /**
+     * on click for our register button
+     * @param evt 
+     */
     private void registerBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registerBtnActionPerformed
         
+        //if not redirected via the basket
         if(!fromBasket)
         {
+            //creates new register form
             Register reg = new Register();
-            reg.setVisible(true);
+            //closes existing form
             this.dispose();
+            //sets new form to visible
+            reg.setVisible(true);
         }
+        //if via basket page
         else
         {
+            //creates new register form passing in from basket boolean and the basket hashmap of orderlines
             Register reg = new Register(fromBasket, basket);
+            //closes existing form
             this.dispose();
+            //sets new form to visible
             reg.setVisible(true);
         }
     }//GEN-LAST:event_registerBtnActionPerformed
 
+    
     /**
      * @param args the command line arguments
      */
@@ -278,6 +339,12 @@ public class CustomerLogin extends javax.swing.JFrame {
         });
     }
 
+    
+    /**
+     * Method defining our pop up box
+     * @param infoMessage - holds the body of our message
+     * @param titleBar  - holds the title of the info box
+     */
     public static void infoBox(String infoMessage, String titleBar)
     {
         JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
